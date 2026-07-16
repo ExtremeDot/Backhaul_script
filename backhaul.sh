7826f7cd1a54f24e27b33eba19771b91fe8152f0
LostInPersia <120102306+ExtremeDot@users.noreply.github.com>
2026-07-16 21:54:05 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -6,5 +6,5 @@ chmod +x /etc/logo2.sh
 if [ -f "backhaul.py" ]; then
     rm backhaul.py
 fi
-wget https://github.com/Azumi67/Backhaul_script/releases/download/script/backhaul.py
+wget https://github.com/ExtremeDot/Backhaul_script/releases/download/script/backhaul.py
 python3 backhaul.py
